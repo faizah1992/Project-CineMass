b490c074c4d0f51b0138172d0beaec1686333336
Rooted1 <ruthobey@gmail.com>
2020-05-05 14:04:01 -0500

    update backend folders

--- cinemass-frontend/src/index.js
+++ cinemass-frontend/src/index.js
@@ -9,22 +9,37 @@ fetch('http://localhost:3000/movies')
     return response.json()
 })
 .then(function(obj){
-
-    obj.forEach(movie =>{
+  obj.forEach(movie =>{
     let movieLi = document.createElement('li')
     movieUl.append(movieLi)
     movieLi.append(movie.title)
 
 
     movieLi.addEventListener('click', function(){
+      movieDiv.remove()
       let divTag = document.createElement('div')
-      divTag.setAttribute('id', 'show-panel')
-      document.body.append(divTag)
+      let imgTag = document.createElement('img')
       let pTag = document.createElement('p')
-      pTag.innerText = movie.details
-      divTag.append(pTag)
-    })
+      let rating = document.createElement('p')
+      let runtime = document.createElement('p')
+      let release_date = document.createElement('p')
+      let backBtn = document.createElement('button')
 
+      imgTag.src = `http://image.tmdb.org/t/p/w185/${movie.image}`
+      pTag.innerText = movie.details
+      rating.innerText = `Rating: ${movie.rating}`
+      runtime.innerText = `Runtime: ${movie.runtime} mins`
+      release_date.innerText = `Release Date: ${movie.released_date}`
+      backBtn.innerText = '<-- Back to All Movies'
+      document.body.append(divTag)
+      divTag.append(imgTag, pTag, rating, runtime, release_date)
+
+      divTag.append(backBtn)
+
+      backBtn.addEventListener('click', function(){
+
+      })
+    })
   })
 
 })
