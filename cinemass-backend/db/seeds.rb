3ae36c573f7bd80c462c990d72fc01c611b5aff2
faizah1992 <faizah1992@gmail.com>
2020-05-07 15:44:16 -0500

    bug fix

--- cinemass-backend/db/seeds.rb
+++ cinemass-backend/db/seeds.rb
@@ -49,7 +49,7 @@ end
   User.create({username: Faker::Name.name, location: Faker::Address.city, bio: Faker::Lorem.paragraph, movie_id: Movie.all.sample.id, image: Faker::Avatar.image, password: "testuser"})
 end
 
-begin
+
 (1..20).each do |num|
   Comment.create({comment: Faker::Lorem.sentence, likes: 0, user_id: User.all.sample.id, movie_id: Movie.all.sample.id})
 end
--- cinemass-frontend/src/navigation.js
+++ cinemass-frontend/src/navigation.js
@@ -2,7 +2,9 @@ let navList = document.querySelectorAll("li")
 
 navList[1].addEventListener('click', function(){
     rootDiv.innerText = ""
+    console.log('Clicked')
       listMovies()
+           
   })
 
 navList[0].addEventListener('click', function(){
